1b8929880b65ff7bcd586624104457b56131e12f
Gerald Mulero <gdmasm@gmail.com>
2025-07-13 01:38:25 -0400

    user model now stores hashed password and adds check_password method for verifying passwords.

--- part3/business/user.py
+++ part3/business/user.py
@@ -1,9 +1,17 @@
 from business.base_model import BaseModel
+import bcrypt
 
 class User(BaseModel):
     def __init__(self, **kwargs):
         self.first_name = kwargs.get("first_name", "")
         self.last_name = kwargs.get("last_name", "")
         self.email = kwargs.get("email", "")
-        self.password = kwargs.get("password", "")
+        password = kwargs.get("password", "")
+        if password and not password.startswith("$2b$"):
+            self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        else:
+            self.password = password
         super().__init__(**kwargs)
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode(), self.password.encode())
