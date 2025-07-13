b7f65bd482ca060981c535202b62e97fb7b02fb4
Gerald Mulero <gdmasm@gmail.com>
2025-07-13 13:19:56 -0400

    user api removes password from all responses and hashes password on creation.

--- part3/presentation/users.py
+++ part3/presentation/users.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restx import Namespace, Resource, fields
 from persistence.repository import InMemoryRepository
+from business.user import User
 
 api = Namespace('users', description='User operations')
 
@@ -20,13 +21,20 @@ repo = InMemoryRepository()
 class UserList(Resource):
     @api.marshal_list_with(user_model)
     def get(self):
-        return repo.get_all("users")
+        users = repo.get_all("users")
+        for user in users:
+        user.pop("password", None)
+        return users
 
     @api.expect(user_model)
     @api.marshal_with(user_model, code=201)
     def post(self):
         data = request.json
-        return repo.save("users", data), 201
+        new_user = User(**data)
+        user_dict = new_user.__dict__.copy()
+        user_dict.pop("password", None)
+        repo.save("users", user_dict)
+        return user_dict, 201
 
 @api.route('/<string:user_id>')
 @api.param('user_id', 'The user ID')
@@ -36,6 +44,7 @@ class User(Resource):
         user = repo.get("users", user_id)
         if not user:
             api.abort(404, "User not found")
+        user.pop("password", None)
         return user
 
     @api.expect(user_model)
@@ -45,6 +54,7 @@ class User(Resource):
         updated_user = repo.update("users", user_id, updates)
         if not updated_user:
             api.abort(404, "User not found")
+        updated_user.pop("password", None)
         return updated_user
 
     def delete(self, user_id):
