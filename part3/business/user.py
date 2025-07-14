0f664d5aac354f1ec4fe882d164d3a0a92a41966
Gerald Mulero <gdmasm@gmail.com>
2025-07-13 22:55:03 -0400

    BaseModel is used from business for a basic database starting point, database connection is brought from app so the code can make and use real tables, and bring in bcrypt library to scramble and check passwords safely for user protection.

--- part3/business/user.py
+++ part3/business/user.py
@@ -1,17 +1,4 @@
 from business.base_model import BaseModel
+from app import db
 import bcrypt
 
-class User(BaseModel):
-    def __init__(self, **kwargs):
-        self.first_name = kwargs.get("first_name", "")
-        self.last_name = kwargs.get("last_name", "")
-        self.email = kwargs.get("email", "")
-        password = kwargs.get("password", "")
-        if password and not password.startswith("$2b$"):
-            self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-        else:
-            self.password = password
-        super().__init__(**kwargs)
-
-    def check_password(self, password):
-        return bcrypt.checkpw(password.encode(), self.password.encode())
