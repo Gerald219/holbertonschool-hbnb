10366d50dfcee75f6599dee2b05090e17b40f948
Gerald Mulero <gdmasm@gmail.com>
2025-07-13 23:02:45 -0400

    business/user.py file, now applied User class using BaseModel for id and timestamps, set table name, and add columns for first_name, last_name, email, password.

--- part3/business/user.py
+++ part3/business/user.py
@@ -2,3 +2,12 @@ from business.base_model import BaseModel
 from app import db
 import bcrypt
 
+class User(BaseModel):
+    __tablename__ = "users"
+
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+
