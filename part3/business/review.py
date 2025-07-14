d833e8017b69a82bdc82c291398c259be7dd5a65
Gerald Mulero <gdmasm@gmail.com>
2025-07-14 19:47:22 -0400

    Review.py  file now uses the database with a reviews table, adds text user_id and place_id as fields, and keeps init to load data from requests.

--- part3/business/review.py
+++ part3/business/review.py
@@ -1,8 +1,16 @@
 from business.base_model import BaseModel
+from app import db
 
 class Review(BaseModel):
+    __tablename__ = "reviews"
+
+    text = db.Column(db.String(1024), nullable=False)
+    user_id = db.Column(db.String(60), nullable=False)
+    place_id = db.Column(db.String(60), nullable=False)
+
     def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.text = kwargs.get("text", "")
         self.user_id = kwargs.get("user_id", "")
         self.place_id = kwargs.get("place_id", "")
-        super().__init__(**kwargs)
+
