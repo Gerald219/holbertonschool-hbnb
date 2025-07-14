f2d3ad79037b62cf9bea714536da7053eadbb5fd
Gerald Mulero <gdmasm@gmail.com>
2025-07-13 23:14:21 -0400

    __init__ in user file, when a new user is made it checks if the password needs locking up, and hashes it if not already safe. It flls in name and email from the info given, and sets is_admin to false.

--- part3/business/user.py
+++ part3/business/user.py
@@ -11,3 +11,15 @@ class User(BaseModel):
     password = db.Column(db.String(256), nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        password = kwargs.get("password", "")
+        if password and not password.startswith("$2b$"):
+            self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        else:
+            self.password = password
+        self.first_name = kwargs.get("first_name", "")
+        self.last_name = kwargs.get("last_name", "")
+        self.email = kwargs.get("email", "")
+        self.is_admin = kwargs.get("is_admin", False)
+
