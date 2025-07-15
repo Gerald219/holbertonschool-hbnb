1ea13d180e9e7477445958d248b7f08202a421e9
Gerald Mulero <gdmasm@gmail.com>
2025-07-14 21:17:28 -0400

    review.py model connects to both user and place using foreign key columns. Makes use of back references so user can access their reviews and place can access its reviews, and keeps the init method to load data from incoming requests.

--- part3/business/review.py
+++ part3/business/review.py
@@ -5,12 +5,14 @@ class Review(BaseModel):
     __tablename__ = "reviews"
 
     text = db.Column(db.String(1024), nullable=False)
-    user_id = db.Column(db.String(60), nullable=False)
-    place_id = db.Column(db.String(60), nullable=False)
+    user_id = db.Column(db.String(60), db.ForeignKey("users.id"), nullable=False)
+    place_id = db.Column(db.String(60), db.ForeignKey("places.id"), nullable=False)
+
+    user = db.relationship("User", backref="reviews")
+    place = db.relationship("Place", backref="reviews")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.text = kwargs.get("text", "")
         self.user_id = kwargs.get("user_id", "")
         self.place_id = kwargs.get("place_id", "")
-
