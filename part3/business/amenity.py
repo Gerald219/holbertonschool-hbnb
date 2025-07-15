2639a6263911da3e0d53a0d0fb7a472058c7e902
Gerald Mulero <gdmasm@gmail.com>
2025-07-14 21:22:07 -0400

    amenity model connects to place using db relationship with secondary set to place amenity table, so each amenity can access its places and each place can list its amenities.

--- part3/business/amenity.py
+++ part3/business/amenity.py
@@ -4,7 +4,8 @@ from app import db
 class Amenity(BaseModel):
     __tablename__ = "amenities"
 
-    name = db.Column(db.String(128), nullable=False)  # name of the amenity, required
+    name = db.Column(db.String(128), nullable=False)
+    places = db.relationship("Place", secondary="place_amenity", backref="amenities")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
