d008b10ff583f5870a5678d430851d571b2d7035
Gerald Mulero <gdmasm@gmail.com>
2025-07-14 21:44:56 -0400

    Amenities table adds id and name fields for storing amenity info, and includes created_at and updated_at columns for time tracking.

--- part3/full_schema.sql
+++ part3/full_schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE reviews (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (place_id) REFERENCES places(id)
 );
+
+CREATE TABLE amenities (
+    id VARCHAR(60) PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME
+);
+
