a3ecd85fe042098cee61707449fb73b1c1b62989
Gerald Mulero <gdmasm@gmail.com>
2025-07-14 22:24:41 -0400

    5 amenities added using insert into amenities with id name and timestamps, includes wifi, air conditioning, pool, washer dryer and parking for testing place to amenity links.

--- part3/full_schema.sql
+++ part3/full_schema.sql
@@ -23,3 +23,22 @@ CREATE TABLE place_amenity (
     FOREIGN KEY (place_id) REFERENCES places(id),
     FOREIGN KEY (amenity_id) REFERENCES amenities(id)
 );
+
+INSERT INTO users (id, first_name, last_name, email, password, is_admin, created_at, updated_at)
+VALUES (
+    '001-gerald',
+    'Gerald',
+    'Mulero',
+    'gdmasm@gmail.com',
+    '$2b$12$6VYpUcRWR7Rb4spDPZgRtuF3RPjhUXINRoXibCCw8Fq8NdAN/3r4q',
+    TRUE,
+    DATETIME('now'),
+    DATETIME('now')
+);
+
+INSERT INTO amenities (id, name, created_at, updated_at) VALUES
+('amenity-001', 'Wi-Fi', DATETIME('now'), DATETIME('now')),
+('amenity-002', 'Air Conditioning', DATETIME('now'), DATETIME('now')),
+('amenity-003', 'Swimming Pool', DATETIME('now'), DATETIME('now')),
+('amenity-004', 'Washer/Dryer', DATETIME('now'), DATETIME('now')),
+('amenity-005', 'Parking', DATETIME('now'), DATETIME('now'));
