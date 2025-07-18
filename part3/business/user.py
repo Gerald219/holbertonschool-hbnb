4172d477ed2507e593e572cfedc7eedf1da2dc7a
Gerald Mulero <gdmasm@gmail.com>
2025-07-18 19:34:11 -0400

    user model with basic personal information, and is_admin flag, added methods to save a hashed password and check if a password is correct using bcrypt.

---
+++ part3/business/user.py
@@ -0,0 +1,17 @@
+from business.base_model import BaseModel
+from app.extensions import db, bcrypt
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def set_password(self, raw_password):
+        self.password = bcrypt.generate_password_hash(raw_password).decode('utf-8')
+
+    def check_password(self, raw_password):
+        return bcrypt.check_password_hash(self.password, raw_password)
