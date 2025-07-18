38a2de1e89eadd53d35fac3d8449341152b16c65
Gerald Mulero <gdmasm@gmail.com>
2025-07-18 18:52:08 -0400

    App factory: defined create_app to load config and activate extensions, now the app can run with database, password hashing, and JWT.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,1 +1,15 @@
-Hello 
+from flask import Flask
+from .config import Config
+from .extensions import db, bcrypt, jwt
+
+def create_app(config_object=Config):
+    """Application factory to set up Flask app and extensions."""
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    return app
