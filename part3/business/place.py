c53c3c472ec7c9cb29f1946a22d9f69f0d1d97aa
Gerald Mulero <gdmasm@gmail.com>
2025-07-18 23:17:35 -0400

    place model includes user_id to link owner and fields for name, description, city, room counts, guest limit, price, and coordinates,, defines the main info needed to store a rental listing.

---
+++ part3/business/place.py
@@ -0,0 +1,17 @@
+from business.base_model import BaseModel
+from app.extensions import db
+
+class Place(BaseModel):
+    __tablename__ = 'places'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    city = db.Column(db.String(128), nullable=False)
+    number_rooms = db.Column(db.Integer, default=0)
+    number_bathrooms = db.Column(db.Integer, default=0)
+    max_guest = db.Column(db.Integer, default=0)
+    price_by_night = db.Column(db.Integer, default=0)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+
