585c7b1fb292aafcaa0776f620eea9e19648b157
Gerald Mulero <gdmasm@gmail.com>
2025-07-19 00:26:59 -0400

    api __init__ file now connects the users and authentication routes through importing their blueprints and registering them so they become active when the app runs.

--- part3/app/api/__init__.py
+++ part3/app/api/__init__.py
@@ -1,5 +1,10 @@
 from .auth import auth_bp
+from .users import users_bp
+
 
 def init_app(app):
     
     app.register_blueprint(auth_bp)
+    
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(users_bp)
