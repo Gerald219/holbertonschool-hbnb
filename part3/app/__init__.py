86596af19ecf115a7ae52ec66679aa3bac1f229e
Gerald Mulero <gdmasm@gmail.com>
2025-07-19 00:09:32 -0400

    App factory uses create_app, this runs db.init_app(app) & bcrypt.init_app, and jwt.init_app.Continues to call init_api(app) from the api module to connect in all the blueprint routes like /login. this makes sure the api works when the app starts.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from .config import Config
 from .extensions import db, bcrypt, jwt
+from .api import init_app as init_api
 
 def create_app(config_object=Config):
-    """Application factory to set up Flask app and extensions."""
     app = Flask(__name__)
     app.config.from_object(config_object)
 
-    
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    
+    init_api(app)
 
     return app
