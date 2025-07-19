51dc81c90290dcf2fb0b59303593f7cd81e30a04
Gerald Mulero <gdmasm@gmail.com>
2025-07-19 01:07:35 -0400

    adds amenities_bp to the api so the app can handle /api/v1/amenities routes for getting, adding, updating, and deleting amenities.

--- part3/app/api/__init__.py
+++ part3/app/api/__init__.py
@@ -2,13 +2,11 @@ from .auth import auth_bp
 from .users import users_bp
 from .places import places_bp
 from .reviews import reviews_bp
+from .amenities import amenities_bp
 
 def init_app(app):
-    
     app.register_blueprint(auth_bp)
-    
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(users_bp
-
-    app.register_blueprint(places_bp))
+    app.register_blueprint(users_bp)
+    app.register_blueprint(places_bp)
     app.register_blueprint(reviews_bp)
+    app.register_blueprint(amenities_bp)
