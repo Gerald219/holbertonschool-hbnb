5b178a430ed53c70f4ea8021cb923231dcd16c9f
Gerald Mulero <gdmasm@gmail.com>
2025-11-02 15:12:53 -0400

    added import for DevConfig and removed if check so the app always loads config by default

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
+from part3.config import DevConfig  # Importing DevConfig
 
-def create_app(config_object=None):
+def create_app(config_object=DevConfig):
     """
     This function makes your Flask app.
     it is a config (settings), loads routes.
     """
     app = Flask(__name__)
-    if config_object is not None:
-        app.config.from_object(config_object)
+    app.config.from_object(config_object)  # Load config settings
 
     @app.route("/")
     def root():
